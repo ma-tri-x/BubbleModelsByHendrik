494c12772151347acf1e0e0f417a85b4c5fff39d
Hendrik Soehnholz <henne-s@gmx.de>
2017-05-13 11:27:20 +0200

    * Add license header to .py files

--- BubbleModels.py
+++ BubbleModels.py
@@ -1,9 +1,26 @@
-"""Solve ODEs of various bubble models
+# Copyright 2017 Hendrik Soehnholz
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+"""Bubble Models
+
+Solve ODEs of various bubble models
 using scipy.integrate.odeint()
 or scipy.integrate.ode
 
-H. Soehnholz
-01.09.2014
+Hendrik Soehnholz
+Initial version: 2014-09-01
 """
 
 import numpy as np
